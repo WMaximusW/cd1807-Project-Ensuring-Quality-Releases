316930623940cdf5053ba3ae6ce48ffb32248a8b
WMaximusW <wnguyenquoctrung1993w@gmail.com>
2024-08-17 21:33:02 +0700

    Update login.py

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -1,19 +1,27 @@
-# #!/usr/bin/env python
+#!/usr/bin/env python
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-
+import os
 
 # Start the browser and login with standard_user
-def login (user, password):
-    print ('Starting the browser...')
-    # --uncomment when running in Azure DevOps.
-    # options = ChromeOptions()
-    # options.add_argument("--headless") 
-    # driver = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome()
-    print ('Browser started successfully. Navigating to the demo page to login.')
+def login(user, password):
+    print('Starting the browser...')
+    
+    # Set the chromedriver path
+    chromedriver_path = "/home/azureuser/.local/lib/python3.6/site-packages/chromedriver_py/chromedriver"
+    
+    options = ChromeOptions()
+    options.add_argument("--headless")  # Run Chrome in headless mode
+    
+    driver = webdriver.Chrome(executable_path=chromedriver_path, options=options)
+    
+    print('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
 
-login('standard_user', 'secret_sauce')
+    # Perform login
+    driver.find_element_by_id('user-name').send_keys(user)
+    driver.find_element_by_id('password').send_keys(password)
+    driver.find_element_by_id('login-button').click()
+    print('Login completed.')
 
-# ToDo: Add more functional UI tests as per your requirements. 
+login('standard_user', 'secret_sauce')
