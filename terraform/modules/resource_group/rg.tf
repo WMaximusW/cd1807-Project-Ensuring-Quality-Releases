3c21a806e506ba1464157daf7ea945255b5c98bf
WMaximusW <wnguyenquoctrung1993w@gmail.com>
2024-08-17 14:59:44 +0700

    Update rg.tf

--- terraform/modules/resource_group/rg.tf
+++ terraform/modules/resource_group/rg.tf
@@ -1,4 +1,12 @@
+# Check if the resource group exists
+data "azurerm_resource_group" "existing" {
+  count = var.create_rg ? 0 : 1
+  name  = var.resource_group
+}
+
+# Create the resource group if it doesn't exist
 resource "azurerm_resource_group" "test" {
-  name     = "${var.resource_group}"
-  location = "${var.location}"
+  count    = var.create_rg ? 1 : 0
+  name     = var.resource_group
+  location = var.location
 }
